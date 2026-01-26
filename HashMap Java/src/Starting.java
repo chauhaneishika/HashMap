7b68da0672600bcc5655460ae8dfc4ef50549284
chauhaneishika <chauhaneishika@gmail.com>
2026-01-26 20:19:02 +0530

    Learning HashMap..

---
+++ HashMap Java/src/Starting.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Starting {
+    public static void main(String args[]){
+        //country(key), population(value)
+        HashMap<String, Integer> map =  new HashMap<>();
+
+        //Insertion
+        map.put("India", 120);
+        map.put("US", 30);
+        map.put("China", 150);
+
+        System.out.println(map);
+
+        //Search
+        if(map.containsKey("China")) {
+            System.out.println("Key is present in the map");
+        } else {
+            System.out.println("Key is not present in the map");
+        }
+        System.out.println(map.get("China")); //key exists
+        System.out.println(map.get("Indonesia")); //key doesn't exist
+    }
+}
