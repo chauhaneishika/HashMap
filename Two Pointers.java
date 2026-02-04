ff4711c4789a0c434cbedd840e1979b60717f15b
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-04 20:06:02 +0530

    Update Two Pointers and add TwoPointers1

--- Two Pointers.java
+++ Two Pointers.java
@@ -1,3 +1,6 @@
-public class Two Pointers {
-    
-}
+//Have two pointers Have
+//Where to apply Two Pointers?
+//1. Sorted Sequence : consider two pointers if the problem involves a sorted sequence
+//2. Compare/Combine Elements: Use two pointers to compare or combine elements from two ends or lists
+//3. Track two positions: Two pointers can efficiently track two positions instead of nested loops
+//4. Move/Shrink Window: Apply two pointers to move or shrink a window for efficient solutions
