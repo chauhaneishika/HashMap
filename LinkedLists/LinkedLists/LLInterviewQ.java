fd02fc4e4c70f1eaf2b37fa002848a3948ac5a1b
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-11 22:40:35 +0530

    LinkedLists Leetcode Questions

--- LinkedLists/LinkedLists/LLInterviewQ.java
+++
@@ -1,7 +0,0 @@
-//package LinkedLists;
-
-public class LLInterviewQ {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
---
+++ LinkedLists/LinkedLists/LinkedLists/LLInterviewQ.java
@@ -0,0 +1,15 @@
+package LinkedLists;
+
+public class LLInterviewQ {
+    //Delete Node
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {val = x;}
+
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
