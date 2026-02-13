ca0d90a864cc4f8aea7a24e22df164771d5a1a85
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-13 21:12:12 +0530

    LinkedLists q

--- LinkedLists/LinkedLists/LinkedLists/FindMiddleElement.java
+++ LinkedLists/LinkedLists/LinkedLists/FindMiddleElement.java
@@ -1,5 +1,22 @@
 package LinkedLists;
 
 public class FindMiddleElement {
-    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) { 
+            this.val = val; 
+        }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+    }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 }
